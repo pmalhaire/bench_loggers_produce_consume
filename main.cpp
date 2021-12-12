12b195207421ca48e99b4c1558a2a03a2ca3a8e4
pmalhaire <pierre.malhaire@gmail.com>
2021-12-12 10:27:49 +0100

    added sync queue

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 #include <condition_variable>
 #include <stdexcept>
-
+#include <queue>
 #include "spdlog/sinks/stdout_sinks.h"
 
 using namespace std;
@@ -16,19 +16,50 @@ auto mainLog = spdlog::stdout_logger_st("main");
 auto produceLog = spdlog::stdout_logger_st("produce");
 auto consumeLog = spdlog::stdout_logger_st("consume");
 
-constexpr auto MESSAGE_COUNT = 10;
+constexpr auto MESSAGE_COUNT = 1000000;
 int consumed = 0;
+
+template <typename T>
+class SynchronizedQueue
+{
+    std::queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable condvar_;
+
+    typedef std::lock_guard<std::mutex> lock;
+    typedef std::unique_lock<std::mutex> ulock;
+
+public:
+    void push(T const &val)
+    {
+        lock l(mutex_);             // prevents multiple pushes corrupting queue_
+        bool wake = queue_.empty(); // we may need to wake consumer
+        queue_.push(val);
+        if (wake)
+            condvar_.notify_one();
+    }
+
+    T pop()
+    {
+        ulock u(mutex_);
+        while (queue_.empty())
+            condvar_.wait(u);
+        // now queue_ is non-empty and we still have the lock
+        T retval = queue_.front();
+        queue_.pop();
+        return retval;
+    }
+};
+
+SynchronizedQueue<int> syncQueue;
 
 void produceThread()
 {
     produceLog->info("start");
     for (int i = 0; i < MESSAGE_COUNT; i++)
     {
-        std::unique_lock<std::mutex> ul(g_mutex);
-        g_ready = true;
-        ul.unlock();
         produceLog->info("Produce one i:{}", i);
-        g_cv.notify_one();
+        syncQueue.push(i);
     }
 }
 
@@ -37,9 +68,8 @@ void consumeThread()
     consumeLog->info("start");
     for (int i = 0; i < MESSAGE_COUNT; i++)
     {
-        std::unique_lock<std::mutex> ul(g_mutex);
-        g_cv.wait(ul, [] { return g_ready; });
-        consumeLog->info("Consuming one");
+        auto elem = syncQueue.pop();
+        consumeLog->info("Consuming one {}", elem);
         consumed++;
     }
 }
@@ -47,16 +77,19 @@ void consumeThread()
 int main()
 {
     mainLog->info("started");
+    auto start = std::chrono::system_clock::now();
     std::thread t1(produceThread);
     std::thread t2(consumeThread);
-    t1.join();
-    t2.join();
     while (consumed < MESSAGE_COUNT)
     {
         mainLog->info("wait for end");
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         mainLog->info("consumed:{}", consumed);
     }
-    mainLog->info("consumed:{}", consumed);
+    t1.join();
+    t2.join();
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end - start;
+    mainLog->info("consumed:{} in {}s rate:{}msg/s", consumed, diff.count(), consumed / diff.count());
     return 0;
 }
